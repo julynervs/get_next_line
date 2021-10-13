d70b5eeb1d5e787862c25277058c908d1423a739
Julyelly Roberta <julyellyr@gmail.com>
2021-10-13 19:28:35 -0300

    "Added null line verification"

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 22:45:29 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/13 19:23:42 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/13 19:28:00 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,12 @@ static int	read_file(int fd, char **buffer, char **buffer_backup, char **line)
 		free(temp);
 	}
 	free(*buffer);
-	*buffer_backup = get_line(*buffer_backup, *line);
+	*buffer_backup = get_line(buffer_backup, line);
+	if (**line == '\0')
+	{
+		free (*line);
+		*line = NULL;
+	}
 	return (bytes_read);
 }
 
@@ -65,7 +70,7 @@ char	*get_next_line(int fd)
 	buffer = malloc(sizeof(char)*(BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	bytes_read = read_file(fd, buffer, buffer_backup, line);
+	bytes_read = read_file(fd, *buffer, *buffer_backup, *line);
 	if (!line && !bytes_read)
 		return (NULL);
 	return (line);
