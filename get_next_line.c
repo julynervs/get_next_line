543566e4057503e3f0b2d3c7f5247712ea6d3c89
Julyelly Roberta <julyellyr@gmail.com>
2021-10-13 19:25:05 -0300

    Changed pointers

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,50 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 22:45:29 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/13 17:19:07 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/13 19:23:42 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*get_line(char *buffer_backup, char *line)
+static char	*get_line(char **buffer_backup, char **line)
 {
 	int	i;
 	char *new_backup;
 
 	i = 0;
-	while (buffer_backup[i] != '\n' && buffer_backup[i] != '\0')
+	while ((*buffer_backup)[i] != '\n' && (*buffer_backup)[i] != '\0')
 		i++;
-	if (buffer_backup[i] == '\n')
+	if ((*buffer_backup)[i] == '\n')
 	{
 		i++;
-		line = ft_substr(buffer_backup, 0, i);
-		new_backup = ft_strdup(&buffer_backup[i]);
+		*line = ft_substr(*buffer_backup, 0, i);
+		new_backup = ft_strdup(*buffer_backup + i);
 	}
 	else
-		line = ft_strdup(buffer_backup);
-	free(buffer_backup);
+		*line = ft_strdup(*buffer_backup);
+	free(*buffer_backup);
 	return (new_backup);
 }
 
-static int	read_file(int fd, char *buffer, char *buffer_backup, char *line)
+static int	read_file(int fd, char **buffer, char **buffer_backup, char **line)
 {
 	int		bytes_read;
 	char	*temp;
 
 	bytes_read = 1;
-	while ((!ft_strchr(buffer_backup, '\n')) && bytes_read)
+	while ((!ft_strchr(*buffer_backup, '\n')) && bytes_read)
 	{
-		if (!buffer_backup)
-			buffer_backup = ft_strdup("");
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		bytes_read = read(fd, *buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
 			return (bytes_read);
-		buffer[bytes_read] = '\0';
-		temp = buffer_backup;
-		buffer_backup = ft_strjoin(temp, buffer);
+		(*buffer)[bytes_read] = '\0';
+		temp = *buffer_backup;
+		*buffer_backup = ft_strjoin(temp, *buffer);
 		free(temp);
 	}
-	free(buffer);
-	buffer_backup = get_line(buffer_backup, line);
+	free(*buffer);
+	*buffer_backup = get_line(*buffer_backup, *line);
 	return (bytes_read);
 }
 
