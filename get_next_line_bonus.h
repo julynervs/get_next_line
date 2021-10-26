30f883047e879694894832d8b85fda58cc442345
Julyelly Roberta <julyellyr@gmail.com>
2021-10-26 10:55:07 -0300

    Added #endif

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,14 +6,16 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 23:06:44 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/26 10:52:29 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/26 10:54:32 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 #ifndef FD_MAX
 # define FD_MAX 256;
+#endif
 
 # include <stdlib.h>
 # include <unistd.h>
