ac7feaaa9b6378de9abd3d270fcaa928770e58a7
Julyelly Roberta <julyellyr@gmail.com>
2021-10-26 11:09:00 -0300

    .

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 23:06:44 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/26 11:07:49 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/26 11:08:41 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <stddef.h>
 
-#ifndef FD_MAX
+# ifndef FD_MAX
 # define FD_MAX 256
 # endif
 
